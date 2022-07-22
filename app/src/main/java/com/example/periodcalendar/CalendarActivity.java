964e9d7777faad128b8ad6e0a4321c9233ae24ab
Rahnama Ahmed <ahmedrahnamachowdhury@gmail.com>
2022-07-22 15:48:06 +0600

    Update CalendarActivity.java
    initial commit

--- app/src/main/java/com/example/periodcalendar/CalendarActivity.java
+++ app/src/main/java/com/example/periodcalendar/CalendarActivity.java
@@ -13,7 +13,6 @@ public class CalendarActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
 
-
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
